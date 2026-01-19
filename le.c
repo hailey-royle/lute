b23f7441eee9559afd62fa713019bf58be1bd135
hailey-royle <hailey.royle@proton.me>
2026-01-18 18:24:15 -0700

    add protection for if *src is \0 DrawLine

--- le.c
+++ le.c
@@ -100,7 +100,7 @@ void WriteFile() {
 }
 
 void DrawLine(char* dst, char* src, int max) {
-        while (max > 0 && *src != '\n') {
+        while (max > 0 && *src != '\n' && *src != '\0') {
                 *dst++ = *src++;
         }
 }
