d72f99b04cbe2f143e21d74cfaac92b785d31b22
hailey-royle <hailey.royle@proton.me>
2026-05-18 16:40:07 -0600

    remove symlinks

--- assert.h
+++ assert.h
@@ -1,1 +1,25 @@
-../personal_libs/assert.h
+#ifndef ASSERT
+#define ASSERT
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Assert( expr, ... ){ \
+	if( !( expr )){ \
+		fprintf( stderr, "%s:%d: %s: Assertion \"%s\" failed. ", __FILE__, __LINE__, __func__, #expr ); \
+		fprintf( stderr, __VA_ARGS__ ); \
+		fprintf( stderr, "\r\n" ); \
+		fflush( stderr ); \
+		abort(); \
+	} \
+}
+
+#define Unreachable() Assert( 0, "Unreachable." );
+
+#define DebugPrintf( ... ){ \
+	fprintf( stderr, "%s:%d: %s ", __FILE__, __LINE__, __func__ ); \
+	fprintf( stderr, __VA_ARGS__ ); \
+	fflush( stderr ); \
+}
+
+#endif
