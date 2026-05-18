264713f1a6bab66cd035f781f5360f6709bfb708
hailey-royle <hailey.royle@proton.me>
2026-05-18 16:14:38 -0600

    utf8 bug fix

--- lute.c
+++ lute.c
@@ -163,15 +163,18 @@ size_t DrawLine( struct String* print, size_t start_index, bool* highlight ){
 		} else if( file.data[ i ] == '\t' ){
 			StringAppend( print, "        ", 8 );
 			tabs++;
+			i++;
 			if( real_end_index - start_index > screen.cols - ( 7 * tabs ) + unicode ){
 				cliped_end_index = start_index + screen.cols - ( 7 * tabs ) + unicode;
 			}
-			i++;
 		} else {
 			size_t i_step = StringUTF8Next( &file, i ) - i;
 			StringAppend( print, &file.data[ i ], i_step );
-			unicode += i_step;
+			unicode += i_step - 1;
 			i += i_step;
+                        if( real_end_index - start_index > screen.cols - ( 7 * tabs ) + unicode ){
+                                cliped_end_index = start_index + screen.cols - ( 7 * tabs ) + unicode;
+                        }
 		}
 		if( inverse_flag ){
 			StringAppend( print, INVERSE_END, sizeof( INVERSE_END ));
